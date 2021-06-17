5f9a82bf6557405d67ec5c6e0fe7a15ab2570b0e
VIKAS RAWAT <66811679+vikasr20012001@users.noreply.github.com>
2021-06-17 14:54:14 +0530

    Add files via upload

---
+++ port.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+print("Content-Type: text/html")    
+print()          
+                   
+
+import cgi
+import subprocess as sub
+cmd = cgi.FieldStorage()
+img=cmd.getvalue("x")
+cont=cmd.getvalue("y")
+st,out=sub.getstatusoutput('sudo docker run -dit --name=' + cont +' '+img)
+if st==0:
+    print("Your Container Successfully Launch : " +out)
+else:
+    print("Error Occurred \n" + out)
+
